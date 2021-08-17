835a81f029353553c7b66a07d53594f90a82c3ef
AmeerAliSalman <bukhariameer5@gmail.com>
2021-08-18 00:37:02 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// Import stylesheets
+import "./style.css";
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from "firebaseui";
+
+// Document elements
+const startRsvpButton = document.getElementById("startRsvp");
+const guestbookContainer = document.getElementById("guestbook-container");
+
+const form = document.getElementById("leave-message");
+const input = document.getElementById("message");
+const guestbook = document.getElementById("guestbook");
+const numberAttending = document.getElementById("number-attending");
+const rsvpYes = document.getElementById("rsvp-yes");
+const rsvpNo = document.getElementById("rsvp-no");
+
+var rsvpListener = null;
+var guestbookListener = null;
+
+async function main() {
+  // Add Firebase project configuration object here
+  // var firebaseConfig = {};
+
+  // firebase.initializeApp(firebaseConfig);
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      }
+    }
+  };
+
+  // const ui = new firebaseui.auth.AuthUI(firebase.auth());
+}
+main();
